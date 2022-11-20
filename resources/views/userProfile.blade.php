b33fde3ac60d640e386700c72453b32ce706a1f5
Shah Foyez <shahfoyez7@gmail.com>
2022-11-20 22:32:01 +0600

    20/11/c2

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -31,7 +31,7 @@
                         <!--begin::Info-->
                         <div class="flex-grow-1">
                             <!--begin::Title-->
-                            <div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
+                            <div class="d-flex justify-content-between align-items-center flex-wrap mb-2">
                                 <!--begin::User-->
                                 <div class="d-flex flex-column">
                                     <!--begin::Name-->
@@ -83,26 +83,7 @@
                                     <!--begin::Stats-->
                                     <div class="d-flex flex-wrap">
 
-                                        <!--begin::Stat-->
-                                        <div class="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-6 mb-3">
-                                            <!--begin::Number-->
-                                            <div class="d-flex align-items-center">
-                                                <!--begin::Svg Icon | path: icons/duotune/arrows/arr065.svg-->
-                                                <span class="svg-icon svg-icon-3 svg-icon-danger me-2">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                        <rect opacity="0.5" x="11" y="18" width="13" height="2" rx="1" transform="rotate(-90 11 18)" fill="black" />
-                                                        <path d="M11.4343 15.4343L7.25 11.25C6.83579 10.8358 6.16421 10.8358 5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75L11.2929 18.2929C11.6834 18.6834 12.3166 18.6834 12.7071 18.2929L18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25C17.8358 10.8358 17.1642 10.8358 16.75 11.25L12.5657 15.4343C12.2533 15.7467 11.7467 15.7467 11.4343 15.4343Z" fill="black" />
-                                                    </svg>
-                                                </span>
-                                                <!--end::Svg Icon-->
-                                                <div class="fs-2 fw-bolder" data-kt-countup="true" data-kt-countup-value="80">0</div>
-                                            </div>
-                                            <!--end::Number-->
-                                            <!--begin::Label-->
-                                            <div class="fw-bold fs-6 text-gray-400">Projects</div>
-                                            <!--end::Label-->
-                                        </div>
-                                        <!--end::Stat-->
+
 
                                     </div>
                                     <!--end::Stats-->
